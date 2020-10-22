c0bba9b239a24e7afb3bd7715c1eef29fd9407ef
Akasarn <Chatree.Akasarn@elektrobit.com>
2020-10-22 16:10:00 +0200

    added initial project structure

---
+++ background_scripts/news_manager.js
@@ -0,0 +1,11 @@
+
+//FETCH DATA FROM DIFFERENT NEWS WEB SITES
+console.log('from news_manager');
+
+fetch(`TODO FIND PROPER API`,
+    {
+        method: 'GET',
+
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));
